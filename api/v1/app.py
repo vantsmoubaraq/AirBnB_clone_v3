eb302982a743f222a4f479f993c800f94713f889
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-02-14 09:19:09 +0300

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 
 """Status of API"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def tear_down(exception):
     """Closes session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """page not found"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
