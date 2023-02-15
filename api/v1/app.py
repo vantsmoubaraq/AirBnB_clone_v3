e0e1693d6fc3397a84e6328b260c931ec0ab6ec5
saad-out <outsaad03@gmail.com>
2023-02-15 19:01:31 +0000

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,13 @@
 """Status of API"""
 
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
