d53e59dbf6e14498c5e9e8afc10a9cb41a2f9461
saad-out <outsaad03@gmail.com>
2023-02-15 15:17:40 +0000

    seperate routes

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -11,31 +11,45 @@ from models.place import Place
 from models.user import User
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET', 'POST'])
 @app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'])
-def places(city_id=None, place_id=None):
+def place(place_id=None):
     """
-    Place model view
+    Place model view based on place_id
     """
-    if place_id:
-        obj = storage.get(Place, place_id)
-        if not obj:
-            abort(404)
-    elif city_id:
-        obj = storage.get(City, city_id)
-        if not obj:
-            abort(404)
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
 
     if request.method == 'GET':
-        if place_id:
-            return jsonify(obj.to_dict())
-        elif city_id:
-            return jsonify([place.to_dict() for place in obj.places])
+        return jsonify(place.to_dict())
 
     elif request.method == 'DELETE':
-        obj.delete()
+        place.delete()
         storage.save()
         return jsonify({})
+
+    elif request.method == 'PUT':
+        if not request.is_json:
+            return jsonify({'error': 'Not a JSON'}), 400
+        for k, v in request.get_json().items():
+            if k not in ['id', 'user_id', 'city_id',
+                         'created_at', 'updated_at']:
+                setattr(place, k, v)
+        place.save()
+        return jsonify(place.to_dict())
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET', 'POST'])
+def city_places(city_id=None):
+    """
+    Place model view based on city_id
+    """
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+
+    if request.method == 'GET':
+        return jsonify([place.to_dict() for place in city.places])
 
     elif request.method == 'POST':
         if not request.is_json:
@@ -52,13 +66,3 @@ def places(city_id=None, place_id=None):
         new_place = Place(**data)
         new_place.save()
         return jsonify(new_place.to_dict()), 201
-
-    elif request.method == 'PUT':
-        if not request.is_json:
-            return jsonify({'error': 'Not a JSON'}), 400
-        for k, v in request.get_json().items():
-            if k not in ['id', 'user_id', 'city_id',
-                         'created_at', 'updated_at']:
-                setattr(obj, k, v)
-        obj.save()
-        return jsonify(obj.to_dict())
